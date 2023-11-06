e613754cfe0f63cf6db9d800021234b5e36b2844
AnnPilchak <anya.pilchak.03@gmail.com>
2023-11-06 17:34:08 +0200

    fixing

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,23 +1,26 @@
 import multer from "multer";
-import path from "path";
 
-const destination = path.resolve("tmp");
+// import path from "path";
+// const destination = path.resolve("tmp");
 
-const storage = multer.diskStorage({
-    destination,
-    filename: (req, file, cb) => {
-        const uniquePreffix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`;
-        const filename = `${uniquePreffix}_${file.originalname}`;
-        cb(null, file.originalname);
-    }
-});
-const limits = {
-    fileSize: 5 * 1024 * 1024
-};
+// const storage = multer.diskStorage({
+//     destination,
+//     filename: (req, file, cb) => {
+//         const uniquePreffix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`;
+//         const filename = `${uniquePreffix}_${file.originalname}`;
+//         cb(null, file.originalname);
+//     }
+// });
+// const limits = {
+//     fileSize: 5 * 1024 * 1024
+// };
+// const upload = multer({
+//     storage,
+//     limits
+// });
 
-const upload = multer({
-    storage,
-    limits
-});
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 
 export default upload;
